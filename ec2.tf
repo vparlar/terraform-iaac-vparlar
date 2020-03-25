8dfafa1f5d1102b05dd597ca548a2dd09877ee30
Vedat Parlar <vedat4evolve@gmail.com>
2020-03-25 05:14:47 +0000

    A

--- ec2.tf
+++ ec2.tf
@@ -4,3 +4,10 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.deployer.key_name}"
 }
+
+resource "aws_instance" "web2" {
+    ami           = "ami-0fc61db8544a617ed"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.deployer.key_name}"
+}
+
