4f258ba09ba6951bf87cf056425a8251e5c741ad
Vedat Parlar <vedat4evolve@gmail.com>
2020-03-29 06:43:10 +0000

    A

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -18,11 +18,5 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
 }
